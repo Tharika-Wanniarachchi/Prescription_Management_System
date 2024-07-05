26758724c140003dfa13ba7fb864d2e3bdcc4fdc
Tharika-Wanniarachchi <147181160+Tharika-Wanniarachchi@users.noreply.github.com>
2024-07-05 16:08:38 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -8,7 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Registration and Login System</title>
+    <title>MediLink</title>
+    <link rel="icon" href="assets/images/logo.png" type="image/x-icon">
 
     <!-- Style CSS -->
     <link rel="stylesheet" href="./assets/css/style.css">
